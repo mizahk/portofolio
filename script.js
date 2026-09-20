c2a28b0ff1d879178f38bd03d8bf4f4c2dd2cf0b
mizahk <mizahafizhzah95@gmail.com>
2026-09-20 23:20:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 function showPage(pageName) {
 
-    // Ambil semua halaman
     const pages = document.querySelectorAll(".page");
 
-    // Sembunyikan semua halaman
     pages.forEach(function(page) {
         page.classList.remove("active");
     });
 
-    // Tampilkan halaman yang dipilih
     const selectedPage = document.getElementById(pageName);
 
     if (selectedPage) {
         selectedPage.classList.add("active");
     }
+
+    closeMenu();
+}
+
+
+/* =========================
+   OPEN MOBILE MENU
+========================= */
+
+function openMenu() {
+
+    const menu = document.getElementById("mobileMenu");
+
+    menu.classList.add("show");
+}
+
+
+/* =========================
+   CLOSE MOBILE MENU
+========================= */
+
+function closeMenu() {
+
+    const menu = document.getElementById("mobileMenu");
+
+    menu.classList.remove("show");
 }
